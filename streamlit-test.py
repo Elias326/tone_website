1f3325446e4478d78ce0b9ac281c6e111a99fef3
Alejandro Pelcastre <alejandro.pelcas@berkeley.edu>
2022-07-31 10:35:35 -0700

    change length of tone again

--- streamlit-test.py
+++ streamlit-test.py
@@ -359,7 +359,7 @@ resource = st.container()
 with header:
     #Insert Tone logo
     image = Image.open('tone_log.png')
-    st.image(image, caption = "Toning down the bad vibes",width=300)
+    st.image(image, caption = "Toning down the bad vibes",width=600)
 
 with mission:
     new_title = '<p style="color:Green;">New image</p>'
