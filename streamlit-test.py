84bafd8116cf36c64948ad90373a8e4670d32482
Alejandro Pelcastre <alejandro.pelcas@berkeley.edu>
2022-08-04 13:49:05 -0700

    slategray

--- streamlit-test.py
+++ streamlit-test.py
@@ -405,7 +405,7 @@ with java:
               var options = {
                 title: 'Tone Representation',
                 pieHole: 0.55,
-                colors: ['#36d8ff', '#529ffc', '#31356e', '#66757f', '#4169E1', '#96DED1']
+                colors: ['#36d8ff', '#529ffc', '#31356e', '#66757f', '#5F9EA0', '#96DED1']
               };
               var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
               chart.draw(data, options);
